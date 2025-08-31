ac73f351973dc855cd9d326f37912f96d71467d4
Ruba <91896958+RubaUser@users.noreply.github.com>
2025-08-31 16:58:20 +0300

    Delete

--- resources/views/admin/viewCategory.blade.php
+++ resources/views/admin/viewCategory.blade.php
@@ -1,7 +1,11 @@
 @extends('admin.maindesign')
 
 @section('viewCategory')
-
+@if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
 <div class="container-fluid">
     <h2>View Categories</h2>
     <table class="table">
@@ -22,11 +26,12 @@
                 <td>{{ $category->category }}</td>
                 <td>{{ $category->created_at }}</td>
                 <td>{{ $category->updated_at }}</td>
-                <td><a href="{{ route('admin.deleteCategory' ,$category->id ) }}" class="btn btn-danger">Delete</a></td>
+                <td><a href="{{ route('admin.deleteCategory' ,$category->id ) }}" onclick="return confirm('Are U Sure ')" class="btn btn-danger">Delete</a></td>
                 <td><a href="" class="btn btn-primary">Edit</a></td>
             </tr>
             @endforeach
         </tbody>
     </table>
 </div>
+
 @endsection
